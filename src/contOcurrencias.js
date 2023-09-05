94af8c43d27d4d014fcc22a98a4fa4ef94acc740
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-09-05 10:13:40 -0400

    separa la cadena por espacio

--- src/contOcurrencias.js
+++ src/contOcurrencias.js
@@ -1,4 +1,15 @@
-function frase(cadena){
+export function frase(cadena){
     return cadena;
 }
-export default frase;
+
+export function contPalabras(cadena){
+    var cad = cadena + "";
+    var contador = 0;
+    for(var i = 0;i<cad.length;i++)
+    {
+        if(cad[i] == " "){
+            contador += 1;
+        }
+    }
+    return contador + 1;
+}
--- src/contOcurrencias.test.js
+++ src/contOcurrencias.test.js
@@ -1,9 +1,11 @@
-import frase from "./contOcurrencias";
+import {frase, contPalabras} from "./contOcurrencias";
 
 describe("Cuenta la cantidad de ocurrencias de una frase", () => {
     it("muestra la frase ingresada por el usuario", () => {
       expect(frase("Esto es una frase de prueba")).toEqual("Esto es una frase de prueba");
     });
-
+    it("separa la frase ingresada tomando como criterio el espacio ' ' ", () => {
+        expect(contPalabras("Esto es una frase de prueba")).toEqual(6);
+    });
   });
   
